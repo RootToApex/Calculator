ef8a8d6106dd3c0409b95c5bfd03797bd84a1c6e
ljh <leejunghyun1019@gmail.com>
2026-01-15 11:27:00 +0900

    첫번쨰, 두번째 숫자 입력칸에 숫자가 아닌것을 넣으면 재실행 되게 만들기, 연산자가 아닌 것을 입력시에 재실행되게 만들기 (try-catch, if 활용)

--- src/step1/App.java
+++ src/step1/App.java
@@ -10,24 +10,44 @@ public class App {
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: "); // println 대신 print 를 사용해 숫자 입력시 바로 옆에 입력하도록 설정
             String input1 = sc.next();
-            int num1 = Integer.parseInt(input1); // 문자를 숫자로 변환
             if (input1.equals("exit")) break;
-            // 양의 정수 입력을 위한 조건
-            if (num1 < 0) {
-                System.out.println("양의 정수를 입력하세요");
-                continue;
+
+            int num1 =0; // 변수를 try-catch 밖에서 미리 생성해서 연산중 오류가 발생하지 않게 하기!
+            try {
+                num1 = Integer.parseInt(input1); // 문자를 숫자로 변환(abc나 !@#의 경우는 컴퓨터가 숫자로 못 바꿈)
+                if (num1 < 0) { // 양의 정수 입력을 위한 조건
+                    System.out.println("양의 정수를 입력하세요");
+                    continue; // 양수가 아니면 재시작
+                }
+            }   catch (NumberFormatException e){
+                    System.out.println("숫자를 입력하세요");
+                    continue; // 숫자가 아니면 재시작
             }
 
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0);
 
+            if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+                System.out.println("사칙연산 기호가 아닙니다 (+, -, *, / 중 하나를 입력하세요)");
+                continue;
+                }
+
+
             System.out.print("두 번째 숫자를 입력하세요: ");
             String input2 = sc.next();
-            int num2 = Integer.parseInt(input2);
-            if (num2 < 0) {
-                System.out.println("양의 정수를 입력하세요");
-                continue;
+            if (input2.equals("exit")) break;
+            int num2 =0;
+            try {
+                num2 = Integer.parseInt(input2); // 문자를 숫자로 변환(abc나 !@#의 경우는 컴퓨터가 숫자로 못 바꿈)
+                if (num2 < 0) { // 양의 정수 입력을 위한 조건
+                    System.out.println("양의 정수를 입력하세요");
+                    continue; // 양수가 아니면 재시작
+                }
+            }   catch (NumberFormatException e){
+                System.out.println("숫자를 입력하세요");
+                continue; // 숫자가 아니면 재시작
             }
+
 
             int result = 0;
 
